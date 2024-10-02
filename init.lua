0b19b70a5f86a3420c8a83fcd4bd218103f70b2d
Joseph Hynimoto Aguilar Lopez <haguilar@auronix.com>
2024-10-02 09:31:28 -0600

    init config

---
+++ init.lua
@@ -0,0 +1,12 @@
+vim.cmd("colorscheme vim")
+local opt = vim.opt 
+
+opt.encoding = 'utf-8'
+opt.number = true 
+opt.autoindent = true 
+opt.wrap = true 
+opt.tabstop = 4 
+opt.ignorecase = true 
+opt.title = true 
+opt.cursorline = false
+opt.shell = 'zsh'
