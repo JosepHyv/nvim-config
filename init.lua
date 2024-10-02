0463d84b77ac90b2e315606b20a77561da9ba6fd
Joseph Hynimoto Aguilar Lopez <haguilar@auronix.com>
2024-10-02 12:05:39 -0600

    adding config for comments bold and green

--- init.lua
+++ init.lua
@@ -1,12 +1,21 @@
 vim.cmd("colorscheme vim")
 local opt = vim.opt 
-
+opt.hlsearch = true 
+opt.ruler = true
+opt.ai = true
 opt.encoding = 'utf-8'
 opt.number = true 
 opt.autoindent = true 
 opt.wrap = true 
-opt.tabstop = 4 
+opt.tabstop = 4
+opt.softtabstop = 4 
+opt.expandtab = true 
 opt.ignorecase = true 
 opt.title = true 
 opt.cursorline = false
 opt.shell = 'zsh'
+opt.mouse = 'a' 
+opt.ignorecase = true 
+opt.shiftwidth = 4
+vim.cmd[[highlight Comment gui=bold ctermbg=10 guifg=LightGreen]]
+vim.cmd[[syntax on]]
