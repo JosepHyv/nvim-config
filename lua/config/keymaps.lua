84e9f8f2e2cf35bdb2776acb6aa6a31229bc5c41
Joseph Hynimoto <josephynimoto@gmail.com>
2025-01-01 03:07:52 -0600

    adding file-explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,22 @@
+local keymap = vim.keymap
+local opts = {noremap = true, silent = true}
 
 -- Formatear automáticamente al guardar
-vim.api.nvim_create_user_command("WF", function()
-  vim.lsp.buf.format({ async = true })
-  vim.cmd("write")  -- Guardar el archivo
-end, {})
+--vim.api.nvim_create_user_command("WF", function()
+--  vim.lsp.buf.format({ async = true })
+--  vim.cmd("write")  -- Guardar el archivo
+--end, {})
+
+
+--vim.api.nvim_create_autocmd("<C-b>", function()
+    
+--end, {})
+
+keymap.set("n", '<C-b>', ':NvimTreeToggle<CR>', opts)
+keymap.set("n", "<C-s>", function()
+    vim.lsp.buf.format({async = true})
+    vim.cmd("write")
+end, opts)
 
 
 --vim.api.nvim_create_autocmd("BufWritePre", {
---
+++ lua/plugins/fileexplorer.lua
@@ -0,0 +1,28 @@
+-- disable netrw at the very start of your init.lua
+
+-- OR setup with some options
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    })
+
+  end,
+}
