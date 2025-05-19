db4bf7a8849eb3d1ec8ae32278f449c64a3c3e64
Joseph Hynimoto Aguilar Lopez <josephynimoto@gmail.com>
2025-05-18 19:42:51 -0600

    improve plugins

--- lua/plugins/fileexplorer.lua
+++ lua/plugins/fileexplorer.lua
@@ -1,6 +1,4 @@
--- disable netrw at the very start of your init.lua
-
--- OR setup with some options
+-- fileexplorer.lua
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -9,22 +7,34 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup({
-      update_cwd = true, 
-      respect_buf_cwd = true, 
+    local nvim_tree = require("nvim-tree")
+
+    nvim_tree.setup({
+      update_cwd = true,
+      respect_buf_cwd = true,
       hijack_directories = {
-        enable = false, 
+        enable = false,
       },
       actions = {
         change_dir = {
-            restrict_above_cwd = true,
-        }, 
+          restrict_above_cwd = true,
+        },
       },
       sort = {
         sorter = "case_sensitive",
       },
       view = {
         width = 30,
+        side = "right", -- 👈 Mueve el árbol a la derecha
+      --   mappings = {
+      --     custom_only = false,
+      --     list = {
+      --       { key = "n", action = "create" },            -- Crear archivo
+      --       { key = "N", action = "create_folder" },     -- Crear carpeta
+      --       { key = "r", action = "rename" },            -- Renombrar
+      --       { key = "d", action = "remove" },            -- Eliminar
+      --     },
+      --   },
       },
       renderer = {
         group_empty = true,
@@ -33,6 +43,6 @@ return {
         dotfiles = true,
       },
     })
-
   end,
 }
+
--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,28 +1,65 @@
 return {
   {
-    "nvimtools/none-ls.nvim", -- ¡Usamos el fork compatible!
+    "nvimtools/none-ls.nvim", -- fork de null-ls
     dependencies = {
       "nvim-lua/plenary.nvim",
       "williamboman/mason.nvim",
-      "jay-babu/mason-null-ls.nvim", -- Conexión de mason con none-ls
+      "jay-babu/mason-null-ls.nvim",
     },
     config = function()
-      local null_ls = require("null-ls") -- el nombre del módulo sigue siendo "null-ls" por compatibilidad
+      local null_ls = require("null-ls")
       local mason_null_ls = require("mason-null-ls")
 
+      local formatting = null_ls.builtins.formatting
+
       null_ls.setup({
+        debug = false,
         sources = {
-          null_ls.builtins.formatting.black,         -- Python
-          null_ls.builtins.formatting.clang_format,  -- C/C++
-          null_ls.builtins.formatting.prettier,      -- JS/TS/etc
+          -- Python
+          formatting.black.with({ extra_args = { "--fast" } }),
+
+          -- Go
+          formatting.gofmt,
+
+          -- C/C++
+          formatting.clang_format.with({
+            filetypes = { "c", "cpp", "objc", "objcpp" },
+          }),
+
+          -- JavaScript, TypeScript, JSX, etc.
+          formatting.prettier.with({
+            filetypes = {
+              "javascript", "javascriptreact",
+              "typescript", "typescriptreact",
+              "json", "yaml", "html", "css", "scss", "markdown"
+            },
+            extra_args = { "--single-quote", "--jsx-single-quote" },
+          }),
+
+          -- Bash
+          formatting.shfmt,
+
+          -- Lua
+          formatting.stylua,
         },
+
+        -- Desactiva el formateo si ya lo provee el LSP
+        on_attach = function(client, _)
+          if client.name == "tsserver" or client.name == "pyright" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end,
       })
 
+      -- Asegura que mason instale los formateadores
       mason_null_ls.setup({
         ensure_installed = {
           "black",
+          "gofmt",
           "clang_format",
           "prettier",
+          "shfmt",
+          "stylua",
         },
         automatic_installation = true,
       })
