a9a05bc302c47a016546ab1390f2d7a7722e0a81
Joseph Hynimoto Aguilar Lopez <josephynimoto@gmail.com>
2025-05-18 19:26:29 -0600

    improve autocmp and ading more langs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,10 +2,10 @@ return {
     {
         "williamboman/mason.nvim",
         config = true,
-        modifiable = true
-    }, 
+        -- modifiable = true
+    },
     {
-        "williamboman/mason-lspconfig.nvim",  
+        "williamboman/mason-lspconfig.nvim",
         dependencies = {
             "williamboman/mason.nvim",
             "neovim/nvim-lspconfig",
@@ -14,48 +14,66 @@ return {
             require("mason").setup()
 
             require("mason-lspconfig").setup({
-                ensure_installed = { "ruff", "ts_ls","clangd", "pyright"}, 
+                ensure_installed = {
+                    "ruff_lsp",
+                    "ts_ls",
+                    "clangd",
+                    "pyright",
+                    "gopls",
+                    "bashls",
+                    "html",
+                    "lua_ls"
+                },
             })
 
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
 
-            lspconfig.ruff.setup{
-            init_options = {
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            autoSearchPaths = true,
+                            diagnosticMode = "openFilesOnly",
+                            useLibraryCodeForTypes = true
+                        }
+                    }
+                }
+            })
+
+            lspconfig.ruff.setup({
+                capabilities = capabilities,
+                init_options = {
                     settings = {
-                        args = {},  
+                        args = {},
                     },
                 },
-            }
-
-            lspconfig.clangd.setup{}
+            })
 
 
             lspconfig.ts_ls.setup({
+                capabilities = capabilities,
                 on_attach = function(client, bufnr)
-                        client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentFormattingProvider = false
                 end,
                 filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
                 root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git")
             })
-                        
-            
-            lspconfig.pyright.setup{
+            lspconfig.clangd.setup({ capabilities = capabilities })
+            lspconfig.gopls.setup({ capabilities = capabilities })
+            lspconfig.bashls.setup({ capabilities = capabilities })
+            lspconfig.html.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
                 settings = {
-                      python = {
-                        analysis = {
-                          autoSearchPaths = true,
-                          diagnosticMode = "openFilesOnly",
-                          useLibraryCodeForTypes = true
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" }
                         }
                     }
-                },
-                on_attach = function(client, bufnr)
-                    client.server_capabilities.documentFormattingProvider = false
-                end, 
-                filetypes = {"python"}
-
-            }
-
+                }
+            })
         end,
     },
 }
